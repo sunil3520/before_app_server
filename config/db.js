77da768cc87918f2cc6ff39588e545069d4a4c83
Sunil Chaudhary <sunilchaudhary7789@gmail.com>
2023-08-03 00:25:53 +0530

    addded

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const { UserModel } = require("../models/user.model");
 dotenv.config();
 
 // const connection = mongoose.connect(process.env.MONGOURL);
 async function connection() {
     await mongoose.connect(process.env.MONGO_URL);
-    mongoose.model('user', schema);
+    mongoose.model('user', UserModel);
   
     await mongoose.model('user').findOne();
   }
